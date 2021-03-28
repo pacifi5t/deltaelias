42d38cf180522414f689f6f801e34b61adc03772
Andrey Dergach <thequasar7@gmail.com>
2021-03-28 18:40:09 +0300

    Implemented gamma & delta codes generation

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,31 @@ fn main() {
     }
 }
 
+fn encode(input: &String, output: &String) {
+    let content = read_from_file(&input).unwrap();
+    let mut char_map: HashMap<char, usize> = HashMap::new(); //K - Symbol, V - Count
+
+    for ch in content.chars() {
+        match char_map.get_mut(&ch) {
+            Some(existing) => *existing += 1,
+            None => {
+                char_map.insert(ch, 1);
+            }
+        }
+    }
+
+    let rank_map = get_rank_map(&char_map);
+    let gamma_map = get_gamma_map(&rank_map);
+    let delta_map = get_delta_map(&gamma_map);
+
+    println!();
+    //TODO: Output in file
+}
+
+fn decode(input: &String, output: &String) {
+    //TODO: Implement decoding
+}
+
 fn read_from_file(path: &String) -> Result<String, io::Error> {
     let mut file = File::open(&path)?;
     let mut content = String::new();
@@ -29,21 +54,49 @@ fn read_from_file(path: &String) -> Result<String, io::Error> {
     Ok(content)
 }
 
-fn encode(input: &String, output: &String) {
-    let content = read_from_file(&input).unwrap();
-    let mut map: HashMap<char, u32> = HashMap::new();
+fn get_rank_map(char_map: &HashMap<char, usize>) -> HashMap<char, usize> {
+    let mut vector: Vec<&char> = char_map.keys().collect();
+    vector.sort_by(|a, b| char_map.get(b).unwrap().cmp(char_map.get(a).unwrap()));
+    let mut rank_map: HashMap<char, usize> = HashMap::new();
+    
+    let mut i = 0;
+    while i < vector.len() {
+        rank_map.insert(*vector[i], i + 1);
+        i += 1;
+    };
 
-    for ch in content.chars() {
-        match map.get_mut(&ch) {
-            Some(existing) => *existing += 1,
-            None => {
-                map.insert(ch, 1);
-            }
-        }
-    }
-    //TODO: Finish encoding
+    rank_map //K - Symbol, V - Rank
 }
 
-fn decode(input: &String, output: &String) {
-    //TODO: Implement decoding
+fn get_gamma_map(rank_map: &HashMap<char, usize>) -> HashMap<usize, String> {
+    let mut gamma_map: HashMap<usize, String> = HashMap::new();
+    for el in rank_map.values() {
+        let zero_count = ((*el as f32).log2() as usize);
+        let mut gamma_code = String::new();
+
+        let mut i = 0;
+        while i < zero_count {
+            gamma_code.push('0');
+            i += 1;
+        }
+        gamma_code.push_str(format!("{:b}", el).as_str());
+        gamma_map.insert(*el, gamma_code);
+    };
+
+    gamma_map //K - Rank, V - Gamma Elias' code
+}
+
+fn get_delta_map(gamma_map: &HashMap<usize, String>) -> HashMap<usize, String> {
+    let mut delta_map: HashMap<usize, String> = HashMap::new();
+    for el in gamma_map.keys() {
+        let temp = ((*el as f32).log2() as usize) + 1;
+        let mut delta_code = String::new();
+        delta_code.push_str(gamma_map.get(&temp).unwrap().as_str());
+        if *el != 1 {
+            delta_code.push_str(&format!("{:b}", el)[1..]);
+        }
+        delta_map.insert(*el, delta_code);
+    };
+    
+    delta_map // K - Rank, V - Delta Elias' code
 }
